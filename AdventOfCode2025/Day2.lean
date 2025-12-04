8f20880baeb64739d609b83202f3a689e30af099
Joseph Mate <mate.joseph@gmail.com>
2025-12-03 22:22:54 -0500

    problem 2 part a complete. it was difficult to get maximum installed. in hindsight, i should have implemented it myself to save time

--- AdventOfCode2025/Day2.lean
+++ AdventOfCode2025/Day2.lean
@@ -37,16 +37,31 @@ private partial def generateErrorsRecurse (current : Int) (exitCondition : Int) 
 private def generateErrors(exitCondition : Int) : List (Int) :=
   generateErrorsRecurse 1 exitCondition []
 
+def between (value : Int) (range : (Int × Int)) : Bool :=
+  let (lower, upper) := range
+  value >= lower && value <= upper
+
+def rangesContain (ranges : List (Int × Int)) (value : Int) : Bool :=
+  ranges.map (between value) |> List.foldl (· || ·) false
+
 def partA (fileSuffix : String) : IO Unit := do
   IO.println "Running Day 2, Part A"
   let ranges ← readInputFile fileSuffix
-  let max ← (ranges.map (·.1) ++ ranges.map (·.2)).maximum?
+  let max := (ranges.map (·.1) ++ ranges.map (·.2)).maximum.get!
+  IO.println s!"ranges"
   IO.println s!"{ranges}"
+
+  let potentialErrors := generateErrors max
+  IO.println s!"potentialErrors"
+  IO.println s!"{potentialErrors}"
+
+  let result := potentialErrors.filter (rangesContain ranges) |> List.foldl (· + ·) 0
+  IO.println s!"result"
+  IO.println s!"{result}"
 
   if fileSuffix.toSlice.contains "sample" then
     let contents ← IO.FS.readFile s!"data/day_02_{fileSuffix}_expected.txt"
     IO.println s!"expected: ({contents})"
-
 
 def partB (fileSuffix : String) : IO Unit := do
   IO.println "Running Day 2, Part B"
@@ -55,5 +70,6 @@ def partB (fileSuffix : String) : IO Unit := do
     let contents ← IO.FS.readFile s!"data/day_02_{fileSuffix}_expected.txt"
     IO.println s!"expected: ({contents})"
 
-#eval
+#eval! do
   partA "sample"
+  partA "real"
