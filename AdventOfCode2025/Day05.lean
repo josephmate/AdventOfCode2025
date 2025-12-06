76d5052610756c864e95d8f597568a0baac79140
Joseph Mate <mate.joseph@gmail.com>
2025-12-05 22:47:34 -0500

    day5 init

---
+++ AdventOfCode2025/Day05.lean
@@ -0,0 +1,40 @@
+import Mathlib.Data.List.MinMax
+import Std.Data.HashSet
+
+namespace Day05
+
+
+private def readInputFile (fileSuffix : String) : IO String := do
+  let fileName := s!"data/day_05_{fileSuffix}.txt"
+  let contents ← IO.FS.readFile fileName
+  return contents
+
+def partA (fileSuffix : String) : IO Unit := do
+  IO.println "Running Day 5, Part A"
+
+  let input ← readInputFile fileSuffix
+  IO.println s!"input:\n{input}"
+
+  let result := 0
+  IO.println s!"result:   {result}"
+
+  if fileSuffix.toSlice.contains "sample" then
+    let contents ← IO.FS.readFile s!"data/day_05_{fileSuffix}_expected_a.txt"
+    IO.println s!"expected: {contents}"
+
+def partB (fileSuffix : String) : IO Unit := do
+  IO.println "Running Day 5, Part B"
+
+  let input ← readInputFile fileSuffix
+  IO.println s!"input:\n{input}"
+
+  let result := 0
+  IO.println s!"result:   {result}"
+
+  if fileSuffix.toSlice.contains "sample" then
+    let contents ← IO.FS.readFile s!"data/day_05_{fileSuffix}_expected_b.txt"
+    IO.println s!"expected: {contents}"
+
+#eval! do
+  partA "sample"
+  partA "real"
