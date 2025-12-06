a8eb74584873a7f14e9beeb891d7c221f0b9c351
Joseph Mate <mate.joseph@gmail.com>
2025-12-05 23:06:31 -0500

    day5 part 1 complete

--- AdventOfCode2025/Day05.lean
+++ AdventOfCode2025/Day05.lean
@@ -4,18 +4,42 @@ import Std.Data.HashSet
 namespace Day05
 
 
-private def readInputFile (fileSuffix : String) : IO String := do
+private def readInputFile (fileSuffix : String) : IO (List (Nat × Nat) × List Nat) := do
   let fileName := s!"data/day_05_{fileSuffix}.txt"
   let contents ← IO.FS.readFile fileName
-  return contents
+  let split := contents.splitOn "\n\n"
+  let header := split[0]!
+  let footer := split[1]!
+  let ranges := header.splitOn "\n"
+  |> List.map (fun val => val.splitOn "-")
+  |> List.map (fun val =>
+    let lower := val[0]!.toNat!
+    let upper := val[1]!.toNat!
+    (lower, upper)
+    )
+  let ingredients := footer.splitOn "\n"
+  |> List.map (fun val => val.toNat!)
+  return (ranges, ingredients)
+
+def withinRanges (ranges: List (Nat × Nat)) (ingredient : Nat) : Bool :=
+  ranges.any (fun range =>
+    let (lower, upper) := range
+    ingredient >= lower && ingredient <= upper
+    )
+
+def findFreshIngredients (ranges: List (Nat × Nat)) (ingredients : List (Nat)): List (Nat) :=
+  ingredients.filter (withinRanges ranges)
 
 def partA (fileSuffix : String) : IO Unit := do
   IO.println "Running Day 5, Part A"
 
   let input ← readInputFile fileSuffix
   IO.println s!"input:\n{input}"
+  let (ranges, ingredients) := input
 
-  let result := 0
+  let freshIngredients := findFreshIngredients ranges ingredients
+
+  let result := freshIngredients.length
   IO.println s!"result:   {result}"
 
   if fileSuffix.toSlice.contains "sample" then
